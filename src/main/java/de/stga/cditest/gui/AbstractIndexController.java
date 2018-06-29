7798a66d198fb1034e8b9726b7e389c8292268b0
JavaTesting1 <40684593+JavaTesting1@users.noreply.github.com>
2018-06-29 11:01:11 +0200

    Improved logging

--- src/main/java/de/stga/cditest/gui/AbstractIndexController.java
+++ src/main/java/de/stga/cditest/gui/AbstractIndexController.java
@@ -5,14 +5,30 @@ import de.stga.cditest.service.AbstractService;
 
 public abstract class AbstractIndexController<S extends SuperParam> {
 
+    private String message;
+
     public void doSomething() {
         S entity = this.getEntity();
         // INTERCEPTION WORKING IN "weld 2" BUT NOT WORKING in "weld 3"
-        this.getCurrentService().doSomething(entity);
+        try {
+            this.getCurrentService().doSomething(entity);
+            this.message = "AssertionError: Exception Expected. Bean has not been intercepted.";
+        } catch (RuntimeException e) {
+            // Expected Exception
+            this.message = "Successfull. Bean has been intercepted.";
+        }
     }
 
     public abstract AbstractService<S> getCurrentService();
 
     public abstract S getEntity();
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
 
 }
